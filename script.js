2075d5f085d3ebbfc1187b9c731e4d8eba2e9461
tearaj <haridasanrajit1@outlook.com>
2023-04-04 22:37:23 +0530

    remove comments

--- script.js
+++ script.js
@@ -6,12 +6,10 @@ const wind_speed = document.getElementById("wind_speed")
 const wind_degrees = document.getElementById("wind_degrees")
 const temp = document.getElementById("temp")
 const humidity = document.getElementById("humidity")
-// const sunrise = document.getElementById("sunrise")
 const min_temp = document.getElementById("min_temp")
 const max_temp = document.getElementById("max_temp")
 const feels_like = document.getElementById("feels_like")
 const cloud_pct = document.getElementById("cloud_pct")
-// const sunset = document.getElementById("sunset")
 
 async function search(event){
 		// console.log(event)
@@ -28,13 +26,10 @@ function display(city, weather){
 	console.log("Called")
 	const weatherEl = document.querySelector(".weather-details")
 	console.log(weather)
-	// weatherEl.textContent = JSON.stringify(weather)
 	wind_speed.innerHTML=`${weather.wind_speed} km/h`
 	wind_degrees.innerHTML=`${weather.wind_degrees}°`
 	temp.innerHTML=`${weather.temp}°C`
 	humidity.innerHTML=`${weather.humidity}%`
-	sunrise.innerHTML=weather.sunrise
-	sunset.innerHTML=weather.sunset
 	min_temp.innerHTML=`${weather.min_temp}°C`
 	max_temp.innerHTML=`${weather.max_temp}°C`
 	feels_like.innerHTML=`${weather.feels_like}°C`
