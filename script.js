17d7f648f6779b0f6829369efb1524be004c2e7f
tearaj <haridasanrajit1@outlook.com>
2023-04-04 23:03:24 +0530

    convert form element to div

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const cloud_pct = document.getElementById("cloud_pct")
 
 async function search(event){
 		// console.log(event)
+		event.preventDefault()
 		const city = searchBar.value
 		searchButton.innerHTML=`<span class="spinner-border spinner-border-sm" style="margin:0 17.3px" role="status" aria-hidden="true"></span>`
 		console.log(searchButton.innerHTML)
@@ -38,5 +39,12 @@ function display(city, weather){
 	cityEl.innerHTML = city.charAt(0).toUpperCase()+city.slice(1);	
 }
 
+//add click event listener to buttons
 searchButton.addEventListener("click",search)
+
+//The below event listener makes sure that the button gets clicked when enter is pressed while in the search bar
+searchBar.addEventListener("keypress",(event)=>{
+	if(event.key=="Enter") {event.preventDefault; searchButton.click()}
+})
+
 display("Chennai", await getWeatherByCity("Chennai"))
