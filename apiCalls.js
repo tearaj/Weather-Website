e9352c30009fa81c02c7fe68f2161b2d0566b8df
tearaj <haridasanrajit1@outlook.com>
2023-04-06 23:03:28 +0530

    remove extra log statements

--- apiCalls.js
+++ apiCalls.js
@@ -12,7 +12,6 @@ export default async function getWeatherByCity(city){
 	try {
 		let res = await fetch(`https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`, options)
 		console.log("API responded data")
-        console.log(res)
 		return {
             data: await res.json(),
             status: res.status
